33cd76a690c2b1715cbb4be54a5ae8c0f8fdfd0a
chelchia <chelseachiajh98@gmail.com>
2018-12-11 20:28:39 +0800

    day 7 complete

--- day7.py
+++ day7.py
@@ -3,39 +3,53 @@ import heapq
 
 filename = "input_day7.txt"
 
+class Node:
+	def __init__(self, node):
+		self.neighbours = dllist()
+		self.prereq = dllist()
+		self.isMarked = False
+		self.time = 60 + ord(node) - 64
+
+	def addDependency(self, nodeTo):
+		self.neighbours.append(nodeTo)
+
+	def addPrereq(self, nodeFrom):
+		self.prereq.append(nodeFrom)
+
+
 class AdjacencyList: 
 	def __init__(self):
 		self.adjlist = {}
 		self.prereq = {}
-		self.isMarked = {}
+		# self.isMarked = {}
 		self.startingNodes = set()
 
 	def addNode(self, node):
-		self.adjlist[node] = dllist()
-		self.isMarked[node] = False
+		self.adjlist[node] = Node(node)
+		# self.isMarked[node] = False
 		self.startingNodes.add(node)
 
 	def addDependency(self, nodeFrom, nodeTo):
 		if nodeFrom not in self.adjlist:
 			self.addNode(nodeFrom)
-		self.adjlist[nodeFrom].append(nodeTo)
 		if nodeTo not in self.adjlist:
 			self.addNode(nodeTo)
 		if nodeTo in self.startingNodes:
 			self.startingNodes.remove(nodeTo)
 
-		if nodeTo not in self.prereq:
-			self.prereq[nodeTo] = dllist()
-		self.prereq[nodeTo].append(nodeFrom)
+		#add dependency
+		self.adjlist[nodeFrom].addDependency(nodeTo)
+		#add prereq
+		self.adjlist[nodeTo].addPrereq(nodeFrom)
 
 	def getDependencies(self, node):
-		return self.adjlist[node]
+		return self.adjlist[node].neighbours
 
 	def isNodeMarked(self, node):
-		return self.isMarked[node]
+		return self.adjlist[node].isMarked
 
 	def markNode(self, node):
-		self.isMarked[node] = True
+		self.adjlist[node].isMarked = True
 
 	def getStartingNode(self):
 		nodes = []
@@ -44,13 +58,22 @@ class AdjacencyList:
 		return nodes
 
 	def isPrereqComplete(self, node):
-		for prereq in self.prereq[node]:
-			if not self.isMarked[prereq]:
+		for prereq in self.adjlist[node].prereq:
+			if not self.adjlist[prereq].isMarked:
 				return False
 		return True
 
 	def printGraph(self):
-		print(self.adjlist)
+		for node in self.adjlist:
+			print(node)
+			print(self.adjlist[node].neighbours)
+
+	def reset(self):
+		for node in self.adjlist:
+			self.adjlist[node].isMarked = False
+
+	def getTime(self, node):
+		return self.adjlist[node].time
 
 def extractDependency(instruction):
 	words = instruction.split(" ")
@@ -69,7 +92,7 @@ if __name__ == "__main__":
 		dep = extractDependency(instruction)
 		graph.addDependency(dep[0], dep[1])
 
-	graph.printGraph()
+	# Topo sort
 	#create heap for traversal of graph. pop from heap. for every node, add neighbours to heap. repeat
 	heap = graph.getStartingNode()
 	for node in heap:
@@ -77,20 +100,64 @@ if __name__ == "__main__":
 	sequence = ""
 	while True:
 		try:
-			# print(heap)
 			currNode = heapq.heappop(heap)
 			graph.markNode(currNode)
 			sequence += currNode
 			for neighbour in graph.getDependencies(currNode):
 				if (not graph.isNodeMarked(neighbour)) and graph.isPrereqComplete(neighbour):
 					#not marked so not traversed, add to queue
-					# print(currNode, neighbour)
 					heapq.heappush(heap, neighbour)
 		except IndexError:
 			print("\nend of heap")
 			break
 
 	print(sequence)
+
+	# ========= Part 2 ==========
+	numWorkers = 5
+	workers = [0 for i in range(numWorkers)]
+	jobs = ['.' for i in range(numWorkers)]
+
+	graph.reset()
+	heap = graph.getStartingNode()
+	time = 0
+	jobsDone = 0
+	while jobsDone < 26:
+		print(time, workers, jobs, heap)
+		#assign jobs to free workers
+		for worker in range(numWorkers):
+			if workers[worker] == 0:
+				try:
+					#if heap is not empty,
+					currNode = heapq.heappop(heap)
+					workers[worker] = graph.getTime(currNode)
+					jobs[worker] = currNode
+					# graph.markNode(currNode)
+				except IndexError:
+					#heap is empty, do nothing. just wait.
+					lol = 1
+
+		#decrement time working for workers
+		for worker in range(numWorkers):
+			if jobs[worker] != '.':
+				#has a job
+				workers[worker] -= 1
+
+		#when job is done, add neighbours is possible
+		for worker in range(numWorkers):
+			if workers[worker] == 0 and jobs[worker] != '.':
+				job = jobs[worker]
+				graph.markNode(job)
+				for neighbour in graph.getDependencies(job):
+					if (not graph.isNodeMarked(neighbour)) and graph.isPrereqComplete(neighbour) and not neighbour in heap:
+						#not marked so not traversed, add to queue
+						heapq.heappush(heap, neighbour)
+				jobs[worker] = '.'
+				jobsDone += 1
+
+		time += 1
+
+	print(time)
 
 
 		
